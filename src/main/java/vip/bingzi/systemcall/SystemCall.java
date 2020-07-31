3e06901e4486f8ed99bb12facd0ba4a54643b42e
冰子 <54745251+BingZi-233@users.noreply.github.com>
2020-07-31 20:40:34 +0800

    冰子 + 增加命令以及权限 + 新增可执行者名单 + 增加了帮助命令 > 对onFileExamine()方法进行了修改
    Signed-off-by: 冰子 <54745251+BingZi-233@users.noreply.github.com>

--- src/main/java/vip/bingzi/systemcall/SystemCall.java
+++ src/main/java/vip/bingzi/systemcall/SystemCall.java
@@ -1,5 +1,6 @@
 package vip.bingzi.systemcall;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,13 +12,17 @@ public final class SystemCall extends JavaPlugin {
     public void onLoad() {//对一些基础文件进行检查
         File fileConfig = new File(getDataFolder(), "config.yml");
         File fileCommand = new File(getDataFolder(), "Command.yml");
-        onFileExamine(fileCommand, false, "命令文件");
+        File fileUserName = new File(getDataFolder(), "username.yml");
         onFileExamine(fileConfig, true, "配置文件");
+        onFileExamine(fileCommand, false, "命令文件");
+        onFileExamine(fileUserName, false, "可执行者名单");
     }
 
     @Override
     public void onEnable() {
-
+        // 抑制对命令注册语句可能为空的检查
+        //noinspection ConstantConditions
+        Bukkit.getPluginCommand("SystemCall").setExecutor(this);
     }
 
     @Override
@@ -34,13 +39,19 @@ public final class SystemCall extends JavaPlugin {
                 if (getDescription().getVersion().equalsIgnoreCase(getConfig().getString("Version"))) {
                     getLogger().info("[ " + fileName + " ]版本与插件版本一致！");
                 } else {
-                    getLogger().warning("配置文件版本与插件版本不一致！");
+                    getLogger().warning("[ " + fileName + " ]版本与插件版本不一致！");
                     if (getConfig().getBoolean("AutoUpDate")) {
+                        getLogger().info("根据用户设定，正在进行自动更换为新版[ " + fileName + " ]");
                         // 抑制对delete()方法检查
                         //noinspection ResultOfMethodCallIgnored
                         file.delete();
                         saveDefaultConfig();
                         reloadConfig();
+                        getLogger().info("[ " + fileName + " ]更换完毕");
+                    } else {
+                        getLogger().warning("根据用户设定，拒绝进行自动更换");
+                        getLogger().warning("请到GitHub上查看并修改配置文件");
+                        getLogger().warning("GitHub：https://github.com/BingZi-233/SystemCall/blob/master/src/main/resources/config.yml");
                     }
                 }
             }
@@ -48,6 +59,8 @@ public final class SystemCall extends JavaPlugin {
             getLogger().warning("未检测到[ " + fileName + " ]存在，正在释放默认配置文件。");
             if (isConfig) {
                 saveDefaultConfig();
+            } else {
+                saveResource(file.getName(), false);
             }
             getLogger().info("[ " + fileName + " ]文件释放完成。");
         }
@@ -55,7 +68,13 @@ public final class SystemCall extends JavaPlugin {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
+        if ("help".equalsIgnoreCase(args[0])) {
+            sender.sendMessage("§a§l§m===========================");
+            sender.sendMessage("  SystemCall - 主命令");
+            sender.sendMessage("    cmd - 以cmd的方式运行");
+            sender.sendMessage("      String - 执行的命令");
+            sender.sendMessage("§a§l§m===========================");
+        }
         return true;
     }
 }
