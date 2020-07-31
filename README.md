ff14e278c70aaa89866ecdf96798c9e271b95a0b
冰子 <54745251+BingZi-233@users.noreply.github.com>
2020-07-31 18:32:24 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SystemCall
+ 一个系统组件调用的Minecraft插件
---
+++ src/main/java/vip/bingzi/systemcall/SystemCall.java
@@ -0,0 +1,17 @@
+package vip.bingzi.systemcall;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SystemCall extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
